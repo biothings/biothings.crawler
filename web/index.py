526727e3af640d0d8b82279290ee2fcda7286d2e
Jerry <xzhou@scripps.edu>
2019-09-21 19:04:47 -0700

    Pass correct content type and enable debugging

--- web/index.py
+++ web/index.py
@@ -1,7 +1,9 @@
 import json
+import logging
 
 import tornado.httpclient
 import tornado.ioloop
+import tornado.options
 import tornado.routing
 import tornado.web
 from bs4 import BeautifulSoup
@@ -16,7 +18,10 @@ class MainHandler(tornado.web.RequestHandler):
 
         url = 'https://www.ncbi.nlm.nih.gov' + self.request.uri
         http_client = tornado.httpclient.AsyncHTTPClient()
-        response = await http_client.fetch(url)
+        response = await http_client.fetch(url, raise_error=False)
+
+        self.set_status(response.code)
+        self.set_header('Content-Type', response.headers.get('Content-Type'))
 
         if self.request.path == '/geo/query/acc.cgi':
             text = response.body.decode()
@@ -25,14 +30,15 @@ class MainHandler(tornado.web.RequestHandler):
             new_tag = soup.new_tag('script', type="application/ld+json")
             new_tag.string = json.dumps(doc, indent=4)
             soup.head.insert(0, new_tag)
-            self.write(soup.prettify())
+            self.finish(soup.prettify())
         else:
-            self.write(response.body)
+            self.finish(response.body)
 
 
 if __name__ == "__main__":
+    logging.getLogger().setLevel('DEBUG')
     application = tornado.web.Application([
         (tornado.routing.AnyMatches(), MainHandler),
-    ])
+    ], debug=True)
     application.listen(8080)
     tornado.ioloop.IOLoop.current().start()
