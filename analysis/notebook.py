570c45b7c863dc82d008bdbaca2b508194dde252
Jerry <xzhou@scripps.edu>
2019-09-26 23:13:05 -0700

    Add stat by field

--- analysis/notebook.py
+++ analysis/notebook.py
@@ -1,3 +1,20 @@
+# %% [markdown]
+# ## Datasets
+# http://su07:9199/_cat/indices?v
+# ### Zenodo
+# http://su07:9199/zenodo/_search?q=*
+# ### Harvard Dataverse
+# http://su07:9199/harvard_dataverse/_search?q=*
+# ### NCBI Geo
+# http://su07:9199/ncbi_geo/_search?q=*
+# ### Omicsdi
+# http://su07:9199/omicsdi/_search?q=*
+
+
+# %% [markdown]
+# ## Question
+# Thread #cvisb andrew  21 days ago
+# > For each repository, show how many datasets have each metadata field populated.
 
 # %%
 from collections import defaultdict, Counter
@@ -6,28 +23,17 @@ from elasticsearch_dsl import Search
 from elasticsearch import Elasticsearch
 from biothings_schema import Schema
 
-# %%
 schema = Schema()
 dataset = schema.get_class("schema:Dataset")
-
-# %%
-properties = [
-    {
-        'name': prop['label'],
-        'types': [kls.name for kls in prop['range']]
-    }
-    for prop in dataset.list_properties(group_by_class=False)
-]
-
-# %%
-property_names = [
+properties = sorted([
     prop['label'] for prop in dataset.list_properties(
         class_specific=False, group_by_class=False)
-]
+])
 
 # %%
 client = Elasticsearch('su07:9199')
-indicies = ('zenodo', 'harvard_dataverse', 'omicsdi')
+indicies = ('zenodo', 'omicsdi', 'harvard_dataverse')
+
 result = defaultdict(partial(defaultdict, Counter))
 
 count = 0
@@ -36,15 +42,20 @@ for index in indicies:
     for doc in search.scan():
         dic = doc.to_dict()
         for key, value in dic.items():
-            if key in property_names:
+            if key in properties:
                 result[index][key][type(value).__name__] += 1
             else:
                 result[index]['__EE__'][type(value).__name__] += 1
         count += 1
         if count % 10000 == 0:
             print(count)
-        # break
 
+# %%
+for index in result:
+    for key in result[index]:
+        result[index][key]['ratio'] = \
+            (sum(result[index][key].values()) /
+             Search(using=client, index=index).count())
 
 # %%
 for index in result:
@@ -55,10 +66,20 @@ for index in result:
             str(result[index][key]['str']),
             str(result[index][key]['list']),
             str(result[index][key]['dict']),
-            "{:7.2%}".format(
-                sum(result[index][key].values()) /
-                Search(using=client, index=index).count())
+            "{:7.2%}".format(result[index][key]['ratio'])
         ]))
+    print()
+
+
+# %%
+print('\t'.join(('properties'.ljust(25), ) + tuple(index.ljust(10) for index in indicies)))
+for property in properties:
+    print(property.ljust(25), end='\t')
+    for index in indicies:
+        if property in result[index]:
+            print("{:<10.2%}".format(result[index][property]['ratio']), end='\t')
+        else:
+            print('-'.ljust(10), end='\t')
     print()
 
 
