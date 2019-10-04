0502a61bea800fda731b95e4e8b7aca8235cdffa
Jerry <xzhou@scripps.edu>
2019-10-03 21:58:17 -0700

    Modify CORS allow headers

--- web/index.py
+++ web/index.py
@@ -97,7 +97,7 @@ class NCBIProxyHandler(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "https://discovery.biothings.io")
-        self.set_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
     def options(self):
