d59de0840a7423872edb86e6ea48e381791b08db
Jerry <xzhou@scripps.edu>
2019-10-31 11:02:26 -0700

    Extend scoll robustness

--- analysis/ncbi_geo_transform.py
+++ analysis/ncbi_geo_transform.py
@@ -6,21 +6,22 @@ from elasticsearch_dsl import Search
 
 from web.index import transform
 
-client = Elasticsearch('localhost:9199')
+client = Elasticsearch('localhost:9200')
 
 
 async def structure_ncbi_geo():
     search = Search(using=client, index='ncbi_geo')
-    for doc in search.scan():
-        dic = doc.to_dict()
+    for doc in search.params(scroll='1d').scan():
         _id = doc.meta.id
-        url = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + _id
-        try:
-            jsld = await transform(dic, url, _id)
-        except Exception as e:
-            client.index(index='ncbi_geo_error', id=_id, body={'exception': str(e)})
-        else:
-            client.index(index='ncbi_geo_transformed', id=_id, body=jsld)
+        if not client.exists(index='ncbi_geo_transformed', id=_id):
+            dic = doc.to_dict()
+            url = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=' + _id
+            try:
+                jsld = await transform(dic, url, _id)
+            except Exception as e:
+                client.index(index='ncbi_geo_error', id=_id, body={'exception': str(e)})
+            else:
+                client.index(index='ncbi_geo_transformed', id=_id, body=jsld)
 
 logging.basicConfig(level='INFO')
 loop = asyncio.get_event_loop()
