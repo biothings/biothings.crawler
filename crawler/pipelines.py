608437d5ffaef47b41f070e2600bdbaceea36eeb
Jerry <xzhou@scripps.edu>
2020-01-22 17:20:54 -0800

    Allow specifying es host and index as envs

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,23 +5,44 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+"""
+Elasticsearch Backend for Scrapy Spider.
+
+* Recommend setting up index mapping ahead of time.
+* Use Env ES_HOST to override default es address.
+* Use Env ES_INDEX to override spider name as index.
+* Require Elasticsearch version ~6.5+.
+* Assume default doc_type or no doc_type.
+
+"""
 import logging
+import os
 
 from elasticsearch import Elasticsearch
-from scrapy.exceptions import DropItem
-
-es = Elasticsearch('localhost:9200')
 
 
 class ESPipeline(object):
+    """
+    Store received items in Elasticsearch.
+    Spider name will be used as the index name.
+    """
+
+    client = Elasticsearch(os.getenv('ES_HOST', 'localhost:9200'))
 
     def process_item(self, item, spider):
+        """
+        Make Elasticsearch index request.
+
+        * Use spider-injected _id field if possible
+          otherwise use @id field in the document
+          generate random id if none of them exist
+
+        """
 
         _id = item.pop('_id', item.get('@id'))
-        # use spider-injected _id field if possible
-        # otherwise use @id field in the document
-        # generate random id if none of them exist
-        res = es.index(index=spider.name, id=_id, body=item)
+        _index = os.getenv('ES_INDEX', spider.name)
+
+        res = self.client.index(index=_index, id=_id, body=item)
         logging.debug(res)
 
         return item
--- crawler/spiders/helper.py
+++ crawler/spiders/helper.py
@@ -1,16 +1,50 @@
+""" Scrapy Spider Mixins """
+
 import logging
 
 from extruct.jsonld import JsonLdExtractor
 
 
 class JsonLdMixin:
+    """
+    Scrapy Spider Mixin Class to Extract JSONLD.
+
+    Example:
+
+    class JSONLDSpider(scrapy.Spider, JsonLdMixin):
+
+        name = 'SPIDER_NAME'
+
+        def start_requests(self):
+
+            urls = []
+            for url in urls:
+                yield scrapy.Request(
+                    url=url,
+                    callback=self.extract_jsonld, # optional
+                    cb_kwargs={
+                        '_id': uuid.uuid1() # optional
+                    })
+    """
 
     def extract_jsonld(self, response, _id=None):
+        """
+        Scrapy Spider Request Callback Function
+
+        * Inject an _id field for database pipeline
+        * Use response URL as default _id
+
+        """
 
         jslds = JsonLdExtractor().extract(response.body)
 
         for jsld in jslds:
             if _id:
                 jsld['_id'] = _id
+            else:
+                jsld['_id'] = response.url
+
             logging.debug(jsld)
             yield jsld
+
+    parse = extract_jsonld  # Set as the default parsing method
