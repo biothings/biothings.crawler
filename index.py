8b8c21abe20dacae536dc5c7152d24838ec5e135
Jerry <xzhou@scripps.edu>
2020-02-20 12:45:34 -0800

    Add simple front page

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from biothings.web.settings import BiothingESWebSettings
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
-from tornado.web import Application
+from tornado.web import Application, StaticFileHandler, RedirectHandler
 
 
 define("port", default="8000", help="local port to run the server")
@@ -14,7 +14,10 @@ options.parse_command_line()
 
 
 API_SETTINGS = BiothingESWebSettings(config='web.config')
-APP_LIST = API_SETTINGS.generate_app_list()
+APP_LIST = API_SETTINGS.generate_app_list() + [
+    (r"/", RedirectHandler, dict(url='/index.html')),
+    (r"/(index.html)", StaticFileHandler, dict(path='static')),
+]
 
 
 def main():
