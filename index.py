23e91697511c7f5f06eff2f7a890b5e40a1e3f50
Marco <artofmarco@gmail.com>
2020-02-25 12:29:26 -0800

    landing page added

--- index.py
+++ index.py
@@ -1,28 +1,42 @@
 '''
     Biothings Crawler API Server
 '''
+import os
 
 from biothings.web.settings import BiothingESWebSettings
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
-from tornado.web import Application, StaticFileHandler, RedirectHandler
+from tornado.web import Application, StaticFileHandler, RedirectHandler, RequestHandler
 
 
 define("port", default="8000", help="local port to run the server")
 options.parse_command_line()
 
 
+public_root = os.path.join(os.path.dirname(__file__), 'static')
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
 API_SETTINGS = BiothingESWebSettings(config='web.config')
 APP_LIST = API_SETTINGS.generate_app_list() + [
-    (r"/", RedirectHandler, dict(url='/index.html')),
-    (r"/(index.html)", StaticFileHandler, dict(path='static')),
+    (r'/', MainHandler),
+    (r'/(.*)', StaticFileHandler, {'path': public_root}),
 ]
+
+settings = dict(
+  debug=True,
+  static_path=public_root,
+  template_path=public_root
+)
 
 
 def main():
 
-    application = Application(APP_LIST)
+    application = Application(APP_LIST, **settings)
 
     server = HTTPServer(application)
     server.bind(options.port)
