c1b9917dfaac30186f54e81676421b71bb72e00d
Marco <artofmarco@gmail.com>
2020-02-26 11:27:43 -0800

    get list from config, added timestanp to axios call, wording

--- index.py
+++ index.py
@@ -9,6 +9,13 @@ from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from tornado.web import Application, StaticFileHandler, RedirectHandler, RequestHandler
 
+import urllib.request
+import imp
+
+file = urllib.request.urlopen('https://raw.githubusercontent.com/flaneuse/biodata-portal/master/discovery/web/siteconfig.py')
+my_code = file.read()
+mymodule = imp.new_module('mymodule')
+exec(my_code, mymodule.__dict__)
 
 define("port", default="8000", help="local port to run the server")
 options.parse_command_line()
@@ -18,8 +25,10 @@ public_root = os.path.join(os.path.dirname(__file__), 'static')
 
 class MainHandler(RequestHandler):
     def get(self):
-        self.render('index.html')
-
+        if mymodule.REPOSITORIES:
+            self.render('index.html',sources=mymodule.REPOSITORIES)
+        else:
+            self.render('index.html',sources='')
 
 API_SETTINGS = BiothingESWebSettings(config='web.config')
 APP_LIST = API_SETTINGS.generate_app_list() + [
