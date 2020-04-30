191e0f661ed22f662096f79fa61d8cffeb206bff
Jerry Zhou <xzhou@scripps.edu>
2020-04-30 14:37:34 -0700

    Update index.py

--- index.py
+++ index.py
@@ -3,21 +3,10 @@
 '''
 
 from biothings.web.index_base import main
-from tornado.web import RedirectHandler, RequestHandler, StaticFileHandler
 from web.handlers import MainHandler
-
-
-# settings = dict(
-#   debug=True,
-#   static_path=public_root,
-#   template_path=public_root
-# )
-
-
 
 
 if __name__ == '__main__':
     main([
     (r'/', MainHandler),
-    # (r'/(.*)', StaticFileHandler, {'path': public_root}),
 ])
