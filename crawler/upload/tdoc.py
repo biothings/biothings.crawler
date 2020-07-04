2a4296271547384dc0a68dc568cdb4f614a38ad0
Zhongchao Qian <zqian@scripps.edu>
2020-07-04 12:22:41 -0700

    Update tdoc.py

--- crawler/upload/tdoc.py
+++ crawler/upload/tdoc.py
@@ -1,4 +1,5 @@
-from typing import Union, Iterable, Dict, Callable, Any, Mapping
+import warnings
+from typing import Union, Iterable, Dict, Callable, Any, Mapping, Container
 
 
 class TransformDoc(dict):
@@ -7,6 +8,26 @@ class TransformDoc(dict):
     """
     def __init__(self, *args, **kwargs):
         super(TransformDoc, self).__init__(*args, **kwargs)
+        for key in self.keys():
+            if type(key) is not str:
+                v = self.pop(key)
+                self[str(key)] = v
+                warnings.warn(f"Key '{key}' is not of type str, casted to str for use in documents.", RuntimeWarning)
+        self._touched_keys = set()
+
+    def __setitem__(self, key, value):
+        super(TransformDoc, self).__setitem__(key, value)
+        self._touched_keys.add(key)
+
+    def update(self, __m: Mapping[str, Any], **kwargs) -> 'TransformDoc':
+        super(TransformDoc, self).update(__m, **kwargs)
+        for k in __m.keys():
+            self._touched_keys.add(k)
+        return self
+
+    def setdefault(self, __key, __default=...):
+        self._touched_keys.add(__key)
+        return super(TransformDoc, self).setdefault(__key, __default)
 
     def rename_keys(self, mappings: Mapping[str, str], ignore_key_error: bool = False) -> 'TransformDoc':
         """Rename keys in a document, returning itself.
@@ -53,9 +74,23 @@ class TransformDoc(dict):
                     raise e
         return self
 
-    def update(self, __m: Mapping[str, Any], **kwargs) -> 'TransformDoc':
-        super(TransformDoc, self).update(__m, **kwargs)
+    def delete_keys_except(self, keep_keys: Container[str]):
+        """Remove keys from document except for the keys given.
+
+        :param keep_keys: Keys to keep.
+        :return: Updated document.
+        """
+        delete_keys = [k for k in self.keys() if k not in keep_keys]
+        for delete_key in delete_keys:
+            del self[delete_key]
         return self
+
+    def delete_unused_keys(self):
+        """Remove keys never written or updated after initialization.
+
+        :return: Updated document.
+        """
+        return self.delete_keys_except(self._touched_keys)
 
     def transform_keys_values(self, mappings: Mapping[str, Callable[[Any], Mapping[str, Any]]],
                               ignore_key_error: bool = False) -> 'TransformDoc':
